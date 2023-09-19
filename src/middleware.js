814919b9c679e50319b6ed7f198c856542d9a357
kakaushow1 <kauani.santos@estudante.ifms.edu.br>
2023-09-19 08:01:09 -0400

    usuarios que não rolou

--- src/middleware.js
+++ src/middleware.js
@@ -6,6 +6,9 @@ export const middleware = (request) => {
 
     const token = request.cookies.get('token')?.value;
     const urlLogin = new URL('/', request.url);
+    const urlPainel = new URL('/pages/dashboard', request.url)
+    const urlRegistrar = new URL('/pages/register', request.url)
+    const urlAlterar = new URL('/pages/alter', request.url)
     const isTokenValidated = validateToken(token);
 
     if (!isTokenValidated || !token) {
@@ -13,9 +16,38 @@ export const middleware = (request) => {
             return NextResponse.redirect(urlLogin);
         }
     }
+
+    if (!isTokenValidated || !token) {
+        if (request.nextUrl.pathname === '/pages/register') {
+            return NextResponse.redirect(urlLogin);
+        }
+    }
+
+    if (!isTokenValidated || !token) {
+        if (request.nextUrl.pathname === '/pages/alter') {
+            return NextResponse.redirect(urlLogin);
+        }
+    }
+    
+    if (isTokenValidated || token) {
+        if (request.nextUrl.pathname === '/') {
+            return NextResponse.redirect(urlPainel);
+        }
+    }
+
+    if (isTokenValidated || token) {
+        if (request.nextUrl.pathname === '/') {
+            return NextResponse.redirect(urlRegistrar);
+        }
+    }
+
+    if (isTokenValidated || token) {
+        if (request.nextUrl.pathname === '/') {
+            return NextResponse.redirect(urlAlterar);
+        }
+    }
     NextResponse.next();
 };
 export const config = {
-    matcher: ['/', '/pages/dashboard']
+    matcher: ['/', '/pages/dashboard', '/pages/register', '/pages/alter']
 };
-
